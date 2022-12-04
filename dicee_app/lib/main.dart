7f007c952463f23560ec2c88632a1b76028fc5ac
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-12-04 07:49:12 +0500

    Lecture 7
    Started working on the app.

--- dicee_app/lib/main.dart
+++ dicee_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -16,6 +17,13 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  var leftDiceNumber = 1;
+
+  int generateRandomNum() {
+    var random = Random();
+    return random.nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,16 +32,28 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(8.0),
-              child: Image.asset(
-                'images/dice1.png',
+              child: GestureDetector(
+                child: Image.asset(
+                  'images/dice$leftDiceNumber.png',
+                ),
+                onTap: () {
+                  leftDiceNumber = generateRandomNum();
+                  print('$leftDiceNumber number');
+                  // print('On tap left dice');
+                },
               ),
             ),
           ),
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(8.0),
-              child: Image.asset(
-                'images/dice1.png',
+              child: GestureDetector(
+                child: Image.asset(
+                  'images/dice2.png',
+                ),
+                onTap: () {
+                  print('On tap right dice');
+                },
               ),
             ),
           ),
