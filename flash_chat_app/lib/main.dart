3c737b33f88e60822f0a2649a0d11c42c3276e57
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2023-01-28 08:44:26 +0500

    Lecture 15
    Initial code setup

---
+++ flash_chat_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat_app/screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
